d101136cebb72a4e7090d74384696716f48b604e
yehanportilla <jancha22@gmail.com>
2019-09-14 23:30:16 -0500

    add class AppConfig

---
+++ src/main/java/com/formacionbdi/springboot/app/item/AppConfig.java
@@ -0,0 +1,21 @@
+package com.formacionbdi.springboot.app.item;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 
+ * @author YehanPortilla
+ *
+ */
+@Configuration
+public class AppConfig {
+
+	@Bean("clienteRest")
+	public RestTemplate registrarRestTemplate() {
+		return new RestTemplate();
+
+	}
+
+}
