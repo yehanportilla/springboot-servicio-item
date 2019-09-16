f7a2aeab0453e5805730c92960b6564254b1516a
yehanportilla <jancha22@gmail.com>
2019-09-15 22:10:26 -0500

    add AppConfig url

--- src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceImpl.java
+++ src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceImpl.java
@@ -1,10 +1,16 @@
 package com.formacionbdi.springboot.app.item.models.service;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.formacionbdi.springboot.app.item.models.Item;
+import com.formacionbdi.springboot.app.item.models.Producto;
+
 /**
  * 
  * @author YehanPortilla
@@ -13,10 +19,17 @@ import com.formacionbdi.springboot.app.item.models.Item;
 @Service
 public class ItemServiceImpl implements ItemService {
 
+	private static final String URL_API_PRODUCTOS = "http://localhost:8001/api/listarProductos";
+
+	@Autowired
+	private RestTemplate clienteRest;
+
 	@Override
 	public List<Item> findAll() {
 
-		return null;
+		List<Producto> productos = Arrays.asList(clienteRest.getForObject(URL_API_PRODUCTOS, Producto[].class));
+
+		return productos.stream().map(p -> new Item(p, 1)).collect(Collectors.toList());
 	}
 
 	@Override
