003ed254e13438fe1023ce0a9c36ec348c52088d
yehanportilla <jancha22@gmail.com>
2019-09-16 22:31:07 -0500

    add controllers and collection postman

---
+++ src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -0,0 +1,38 @@
+package com.formacionbdi.springboot.app.item.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.formacionbdi.springboot.app.item.models.Item;
+import com.formacionbdi.springboot.app.item.models.service.ItemService;
+
+/**
+ * 
+ * @author YehanPortilla
+ *
+ */
+@RestController
+@RequestMapping("/api")
+public class ItemController {
+
+	@Autowired
+	private ItemService itemService;
+
+	@GetMapping("/listaItems")
+	public List<Item> listar() {
+		return itemService.findAll();
+
+	}
+
+	@GetMapping("/detalle/{id}/cantidad/{cantidad}")
+	public Item buscarPorId(@PathVariable Long id, @PathVariable Integer cantidad) {
+
+		return itemService.findById(id, cantidad);
+	}
+
+}
--- src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceImpl.java
+++ src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceImpl.java
@@ -1,7 +1,9 @@
 package com.formacionbdi.springboot.app.item.models.service;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,23 +21,36 @@ import com.formacionbdi.springboot.app.item.models.Producto;
 @Service
 public class ItemServiceImpl implements ItemService {
 
-	private static final String URL_API_PRODUCTOS = "http://localhost:8001/api/listarProductos";
+	private static final String URL_API_LISTA_PRODUCTOS = "http://localhost:8001/api/listarProductos";
+
+	private static final String URL_API_BUSCA_POR_ID_PRODUCTO = "http://localhost:8001/api/buscarProducto/{id}";
 
 	@Autowired
 	private RestTemplate clienteRest;
 
+	/**
+	 * Method list product of microservicio productos
+	 */
 	@Override
 	public List<Item> findAll() {
 
-		List<Producto> productos = Arrays.asList(clienteRest.getForObject(URL_API_PRODUCTOS, Producto[].class));
-
+		List<Producto> productos = Arrays.asList(clienteRest.getForObject(URL_API_LISTA_PRODUCTOS, Producto[].class));
 		return productos.stream().map(p -> new Item(p, 1)).collect(Collectors.toList());
 	}
 
+	/**
+	 * Method search by id product
+	 */
 	@Override
 	public Item findById(Long id, Integer cantidad) {
 
-		return null;
+		Map<String, String> pathVariables = new HashMap<String, String>();
+		
+		pathVariables.put("id", id.toString());
+
+		Producto producto = clienteRest.getForObject(URL_API_BUSCA_POR_ID_PRODUCTO, Producto.class, pathVariables);
+
+		return new Item(producto, cantidad);
 	}
 
 }
