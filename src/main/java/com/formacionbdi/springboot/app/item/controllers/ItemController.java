11b553099a994a8f4850a194836765777b82668a
yehanportilla <jancha22@gmail.com>
2019-10-02 16:49:59 -0500

    se agregan metodos eliminar crear y actualizar y se remplaza serviceFeingn por serviceRestTemplate

--- src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -13,9 +13,14 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.springboot.app.item.models.Item;
@@ -39,7 +44,7 @@ public class ItemController {
 	private Environment env;
 
 	@Autowired
-	@Qualifier("serviceFeign")
+	@Qualifier("serviceRestTemplate")
 	private ItemService itemService;
 
 	/**
@@ -98,13 +103,47 @@ public class ItemController {
 		json.put("puerto", puerto);
 
 		if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) {
-			
+
 			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
 			json.put("autor.email", env.getProperty("configuracion.autor.email"));
-
 		}
 
 		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+	}
+
+	/**
+	 * Metodo para crear el producto desde servicio items
+	 * @param producto
+	 * @return
+	 */
+	@PostMapping("/crear")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto crear(@RequestBody Producto producto) {
+
+		return itemService.save(producto);
+	}
+	
+	
+	/**
+	 * Metodo encargado de actualizar el producto des de servicio items
+	 * @return
+	 */
+	@PutMapping("/editar/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto editar (@RequestBody Producto producto,@PathVariable Long id) {
+
+		return itemService.update(producto, id);
+	} 
+	
+	/**
+	 * Metodo encargado de eliminar el producto des de el servicio items
+	 * @param id
+	 */
+	@DeleteMapping("/eliminar/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void eliminar(@PathVariable Long id) {
+		
+		itemService.delete(id);
 	}
 
 }
