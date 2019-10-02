5c5c92c9335f4246cd64d2074c937cb1e70317f8
yehanportilla <jancha22@gmail.com>
2019-10-02 09:25:09 -0500

    leer configuracion desde controlador utilizando anotacion @value

--- src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -1,9 +1,16 @@
 package com.formacionbdi.springboot.app.item.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,43 +30,69 @@ import com.netflix.ribbon.proxy.annotation.Hystrix;
 @RestController
 public class ItemController {
 
+	private static Logger log = LoggerFactory.getLogger(ItemController.class);
+
 	@Autowired
 	@Qualifier("serviceFeign")
 	private ItemService itemService;
+
+	/**
+	 * Inyectamos configuracion de properties de la carpeta config del archivo
+	 * servicio-items-properties
+	 */
+	@Value("${configuracion.texto}")
+	private String texto;
 
 	@GetMapping("/listaItems")
 	public List<Item> listar() {
 		return itemService.findAll();
 
 	}
-	
-    @HystrixCommand(fallbackMethod = "metodoAlternativo")// Camino alternativo
+
+	@HystrixCommand(fallbackMethod = "metodoAlternativo") // Camino alternativo
 	@GetMapping("/detalle/{id}/cantidad/{cantidad}")
 	public Item buscarPorId(@PathVariable Long id, @PathVariable Integer cantidad) {
 
 		return itemService.findById(id, cantidad);
 	}
-    
-    
-    /**
-     * Method alternative for error
-     * @param id
-     * @param cantidad
-     * @return
-     */
-    public Item metodoAlternativo (Long id,Integer cantidad) {
-    	Item item = new Item();
-    	Producto producto = new Producto();
-    	
-    	item.setCantidad(cantidad);
-    	producto.setId(id);
-    	producto.setNombre("Camara Sony");
-    	producto.setPrecio(500.00);
-    	item.setProducto(producto);
-    	
-    	return item;
-    }
-    
-    
+
+	/**
+	 * Method alternative for error
+	 * 
+	 * @param id
+	 * @param cantidad
+	 * @return
+	 */
+	public Item metodoAlternativo(Long id, Integer cantidad) {
+		Item item = new Item();
+		Producto producto = new Producto();
+
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("Camara Sony");
+		producto.setPrecio(500.00);
+		item.setProducto(producto);
+
+		return item;
+	}
+
+	/**
+	 * Metodo para mostrar como respuesta la configuracion de servidor en un handler
+	 * 
+	 * @param puerto
+	 * @return ResponseEntity
+	 */
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto) {
+
+		log.info(texto);
+
+		Map<String, String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+
+	}
 
 }
