72fcf2d2545d1acb9d535cd862867c7690b8021c
yehanportilla <jancha22@gmail.com>
2019-10-02 10:20:32 -0500

    configurando profiles dev y prod en servicio items

--- src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,6 +32,9 @@ import com.netflix.ribbon.proxy.annotation.Hystrix;
 public class ItemController {
 
 	private static Logger log = LoggerFactory.getLogger(ItemController.class);
+
+	@Autowired
+	private Environment env;
 
 	@Autowired
 	@Qualifier("serviceFeign")
@@ -91,8 +95,14 @@ public class ItemController {
 		json.put("texto", texto);
 		json.put("puerto", puerto);
 
-		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+		if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) {
+			
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
 
+		}
+
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}
 
 }
